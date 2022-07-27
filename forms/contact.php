d3b3af299d4a52d368f60d42edede09cfdb23924
Christian Tampus <tanshongshongy@gmail.com>
2022-07-27 22:19:18 +0800

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -9,33 +9,51 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'tanshongshongy@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  //   include( $php_email_form );
+  // } else {
+  //   die( 'Unable to load the "PHP Email Form" Library!');
+  // }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  // // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-  // $contact->smtp = array(
-  //   'host' => 'smtp.gmail.com',
-  //   'username' => 'christian.tampus@cmu.edu.ph',
-  //   'password' => '',
-  //   'port' => '587'
-  // );
+  // // $contact->smtp = array(
+  // //   'host' => 'smtp.gmail.com',
+  // //   'username' => 'christian.tampus@cmu.edu.ph',
+  // //   'password' => '',
+  // //   'port' => '587'
+  // // );
   
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  // echo $contact->send();
+
+  if(isset($_POST['submit'])){
+    $to = "tanshongshongy@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $subject2 = "Copy of your form submission to Christian Tampus Web CV";
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+
 ?>
